cd41cefc6a26e4c7a0775272691b6f592797e851
npolshakova <nina.polshakova@solo.io>
2026-01-14 11:17:52 -0500

    fix kind req
    Signed-off-by: npolshakova <nina.polshakova@solo.io>

--- crates/agentgateway/src/types/agent_xds.rs
+++ crates/agentgateway/src/types/agent_xds.rs
@@ -563,29 +563,11 @@ impl TryFrom<&proto::agent::Route> for (Route, ListenerKey) {
 	type Error = ProtoError;
 
 	fn try_from(s: &proto::agent::Route) -> Result<Self, Self::Error> {
-		fn route_kind_from_key<'a>(key: &'a str, namespace: &str, name: &str) -> Option<&'a str> {
-			let segs: Vec<&str> = key.split('/').filter(|s| !s.is_empty()).collect();
-			if segs.len() < 3 {
-				return None;
-			}
-			for i in 1..segs.len() - 1 {
-				if segs[i] == namespace && segs[i + 1] == name {
-					return Some(segs[i - 1]);
-				}
-			}
-			None
-		}
-
-		let mut name: RouteName = s
+		let name: RouteName = s
 			.name
 			.as_ref()
 			.ok_or(ProtoError::MissingRequiredField)?
 			.into();
-		if name.kind.is_none()
-			&& let Some(kind) = route_kind_from_key(&s.key, name.namespace.as_str(), name.name.as_str())
-		{
-			name.kind = Some(strng::new(kind));
-		}
 		let r = Route {
 			key: strng::new(&s.key),
 			name,
