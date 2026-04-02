9e726dbd692faf51787231e79f110de398244ed0
John Howard <john.howard@solo.io>
2026-04-01 19:38:27 -0700

    cleanup misc hyper-util cruft that is unused (#1431)

--- crates/agentgateway/src/client/mod.rs
+++ crates/agentgateway/src/client/mod.rs
@@ -389,7 +389,7 @@ impl Client {
 			backend_config: Arc::new(backend_config),
 			metrics,
 		};
-		let client = b.build_with_pool_key(connector.clone());
+		let client = b.build(connector.clone());
 		Client { client, connector }
 	}
 
