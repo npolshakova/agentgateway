3fb00055c4523174a10fbe3b80d0a8acd697affc
Steven Landow <steven@landow.dev>
2026-04-23 23:51:08 +0800

    fix(cel): properties list drops segment after bracket accessor (#1638)
    For the expression: `a["b"].c`
    
    Before:
    
    ```
    Select(field="c")		path=[]
    	Call(fn="_[_]")		push "c", path=["c"]
    		Ident("a")		push "a", emit ["a","c"], clear
    		Literal("b")	ignored
    
    Out: [["a", "c"]]
    ```
    
    After:
    
    <pre>
    Select(field="c")		path=[]
    	Call(fn="_[_]")		push "c"     → path=["c"]
    		Ident("a")		<b>clear()</b>, push "a", emit ["a"], clear
    		Literal("b")	ignored
    
    Out: [["a"]]
    
    </pre>
    
    Signed-off-by: Steven Landow <steven@landow.dev>

--- crates/agentgateway/src/cel/tests.rs
+++ crates/agentgateway/src/cel/tests.rs
@@ -323,9 +323,11 @@ fn test_properties() {
 	test(r#"!a.b"#, &["a.b"]);
 	test(r#"a.b < c"#, &["a.b", "c"]);
 	test(r#"a.b + c + 2"#, &["a.b", "c"]);
-	// This is not right! Should just be 'a' probably
-	test(r#"a["b"].c"#, &["a.c"]);
+	test(r#"a["b"].c"#, &["a"]);
+	test(r#"a["b"]["c"]"#, &["a"]);
 	test(r#"a.b[0]"#, &["a.b"]);
+	test(r#"a.b[0].c"#, &["a.b"]);
+	test(r#"a[b.c]"#, &["a", "b.c"]);
 	test(r#"{"a":"b"}.a"#, &[]);
 	// Test extauthz namespace recognition
 	test(r#"extauthz.user_id"#, &["extauthz.user_id"]);
