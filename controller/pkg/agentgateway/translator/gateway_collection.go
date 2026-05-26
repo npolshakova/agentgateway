51dfbc883f03ad7ba122a85e555863485ca43ed4
John Howard <john.howard@solo.io>
2026-05-26 12:56:35 -0700

    controller: minor micro-optimizations (#1942)
    Speed up two hot paths
    
    Signed-off-by: John Howard <john.howard@solo.io>

--- controller/pkg/agentgateway/translator/gateway_collection.go
+++ controller/pkg/agentgateway/translator/gateway_collection.go
@@ -2,6 +2,7 @@ package translator
 
 import (
 	"bytes"
+	"cmp"
 	"context"
 	"fmt"
 	"strings"
@@ -328,11 +329,14 @@ func GatewayTransformationFunc(cfg GatewayCollectionConfig) func(ctx krt.Handler
 		// - ListenerSet ordered alphabetically by “{namespace}/{name}”
 		slices.SortFunc(listenersFromSets, func(a, b ListenerSet) int {
 			// primary sort: creation timestamp (oldest first)
-			if cmp := a.ParentInfo.CreationTimestamp.Compare(b.ParentInfo.CreationTimestamp.Time); cmp != 0 {
-				return cmp
+			if r := a.ParentInfo.CreationTimestamp.Compare(b.ParentInfo.CreationTimestamp.Time); r != 0 {
+				return r
 			}
 			// secondary sort: alphabetically by "{namespace}/{name}"
-			return strings.Compare(a.Parent.String(), b.Parent.String())
+			if r := cmp.Compare(a.Parent.Namespace, b.Parent.Namespace); r != 0 {
+				return r
+			}
+			return cmp.Compare(a.Parent.Name, b.Parent.Name)
 		})
 
 		for _, ls := range listenersFromSets {
@@ -381,20 +385,19 @@ const (
 func validateListenerConflicts(listeners []*GatewayListener) {
 	portMap := make(map[gwv1.PortNumber]*portProtocol)
 	for _, listener := range listeners {
-		hset := sets.New(listener.ParentInfo.Hostnames...)
 		if p, ok := portMap[listener.ParentInfo.Port]; ok {
 			if p.protocol == listener.ParentInfo.Protocol {
-				if p.hostnames.Intersection(hset).Len() == 0 {
-					p.hostnames = p.hostnames.Union(hset)
-				} else {
+				if slices.ContainsFunc(listener.ParentInfo.Hostnames, p.hostnames.Contains) {
 					listener.Conflict = ListenerConflictHostname
+				} else {
+					p.hostnames.InsertAll(listener.ParentInfo.Hostnames...)
 				}
 			} else {
 				listener.Conflict = ListenerConflictProtocol
 			}
 		} else {
 			portMap[listener.ParentInfo.Port] = &portProtocol{
-				hostnames: hset,
+				hostnames: sets.New(listener.ParentInfo.Hostnames...),
 				protocol:  listener.ParentInfo.Protocol,
 			}
 		}
